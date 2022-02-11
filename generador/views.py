6c4d5a6784895d7375b8b8cacff2ecedecdf9048
Dayrons <dayronstovar@gmail.com>
2022-02-11 13:37:46 -0400

    estructurando directorios

--- generador/views.py
+++ generador/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from generador.models import Cupon
+
 # Create your views here.
 
 def index(request):
+    """ cupon = Cupon(empresa='empresa1', codigo='123*+-', valor=10, usado=False)
+    cupon.save() """
+
+    cupones = Cupon.objects.all()
+
+    print(cupones)
+    
     return render(request,'index.html')
